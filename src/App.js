58c9de6b9b15cb7128980de8b59cdb63168dc00b
Maksim Shastin <maximym384.sh@gmail.com>
2024-05-11 18:39:31 +0300

    TabsChanged

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import { Typography } from '@mui/material';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavigationMenu from './Pages/NavigationMenu';
-import SettignsPage from './Pages/Options';
-import ViewMode from './Pages/ViewMode';
-function App() {
+import Pages from './Pages/Options';
+export default function App() {
   return (
     <header>
-    <Typography sx={{backgroundColor:'DarkSeaGreen'}}>Made by Maksim Shastin</Typography>
-    <BrowserRouter>
-          <NavigationMenu/>      
-          <Routes>
-            <Route path="/Options" element={<SettignsPage />} />
-            <Route path="/ViewMode" element={<ViewMode />} />
-          </Routes>
-    </BrowserRouter>
+      <Typography sx={{backgroundColor:'DarkSeaGreen'}}>Made by Maksim Shastin</Typography>
+      <Pages/>
     </header>
   );
 }
-
-export default App;
